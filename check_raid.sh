6d165f01516ab1afb1b7a47e7f8ddc466e169fcc
Simon Struck <simon@simonscode.org>
2019-08-14 00:06:56 +0200

    Add url variable

--- check_raid.sh
+++ check_raid.sh
@@ -37,7 +37,7 @@ EOM
 
 
 if ! diff ${DATA_LOCATION}/last_report ${TODAYS_REPORT}; then
-   openssl s_client -crlf -quiet -starttls smtp -connect email-smtp.eu-west-1.amazonaws.com:587 < ${MESSAGE}
+   openssl s_client -crlf -quiet -starttls smtp -connect ${AMAZONSES_URL} < ${MESSAGE}
    rm -f ${DATA_LOCATION}/last_report
    ln -s ${TODAYS_REPORT} ${DATA_LOCATION}/last_report
 else
